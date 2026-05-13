76bbf4a519d5395cbacedbb17d8a57cf0aa63097
Kevin <kevinkjeyovjec@gmail.com>
2026-05-13 15:00:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import Features from './pages/Features'
+import Home from './pages/Home'
+import Community from './pages/Community'
+import Blog from './pages/Blog'
+import Pricing from './pages/Pricing'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link> 
+          <Link to="/Features">Features</Link>
+          <Link to="/Community">Community</Link>
+          <Link to="/Blog">Blog</Link>
+          <Link to="/Pricing">Pricing</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Features" element={<Features />} />
+          <Route path="/Community" element={<Community/>} />
+          <Route path="/Blog" element={<Blog />} />
+          <Route path="/Pricing" element={<Pricing />} />
+        </Routes>
+      </BrowserRouter>   
+    </>
+  )
+}
+
+export default App
