97d287490e2afefaa2ad91d079fe0255ba736742
Kevin <kevinkjeyovjec@gmail.com>
2026-05-14 14:22:43 +0530

    'DiscoverCSSEdited'

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -3,9 +3,9 @@ function Discover() {
   return (
     <section className="Discover">
       <h1>Discover the Power of Nexcent</h1>
-      <p>Unlock your team's full potential with our suite of modern tools designed for collaboration, community management, and growth.</p>
-      <button>Get Started for Free</button>
-    </section>
+      <p className="para">Unlock your team's full potential with our suite of modern tools designed for collaboration, community management, and growth.</p>
+      <a href="/register"><button>Register</button>
+</a>    </section>
   );
 }
 
