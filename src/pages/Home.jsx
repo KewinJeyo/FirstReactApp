debb3e0d50dc76ce730b33bfd827c0cf8fccbeb9
Kevin <kevinkjeyovjec@gmail.com>
2026-05-14 09:56:11 +0530

    'Added_css_to_home'

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 
 function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  )
+    return (
+        <div>
+            <section className='landing'>
+                <div className='title'>
+                <h1>Lessons and insights <br />
+                    <span className='span'>from 8 years</span></h1>
+                </div>
+                <p>Where to grow your business as a photographer: site or social media?</p>
+                <button>Register</button>
+            </section>
+            <div className='ourclients'>
+                <h1>Our Clients</h1>
+                <p>We have been working with some Fortune 500+ clients</p>
+            </div>
+
+            <div className='manage'>
+                <h1>Manage your entire community in a single system</h1>
+                <p>Who is Nextcent suitable for?</p>
+            </div>
+
+        </div>
+    )
 }
 
 export default Home
